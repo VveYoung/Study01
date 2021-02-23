c2ef3a354e7b29e3a8c80f28df0b50f293b67cef
VveYoung <563244723@qq.com>
2021-02-23 21:12:20 +0800

    nihao

---
+++ src/main/java/com/cwy/Hello.java
@@ -0,0 +1,13 @@
+package com.cwy;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("zhanhan,nihao");
+        System.out.println("zhanhan,nihao");
+        System.out.println("zhanhan,nihao9");
+
+    }
+
+}
